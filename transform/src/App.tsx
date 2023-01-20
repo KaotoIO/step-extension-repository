1115409a6490f3546a4e12a2d1c1590f0bb7ef88
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-01-20 09:55:51 -0500

    feat(eip): Add expression syntax selector on transform EIP (KaotoIO/kaoto-ui#1080)

---
+++ transform/src/App.tsx
@@ -0,0 +1,21 @@
+import TransformStep from "./Components/TransformStep";
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        </p>
+      </header>
+      <TransformStep />
+    </div>
+  );
+}
+
+export default App;
