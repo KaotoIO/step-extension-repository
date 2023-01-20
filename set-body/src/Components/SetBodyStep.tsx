976ed6247e3285696e336dab369c87b0f497d898
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-01-20 10:00:54 -0500

    feat(eip): Add expression syntax selector on set-body EIP (KaotoIO/kaoto-ui#1080)

---
+++ set-body/src/Components/SetBodyStep.tsx
@@ -0,0 +1,68 @@
+import {Expression} from './Expression';
+import {ActionGroup, Button, Form } from '@patternfly/react-core';
+import {useState} from 'react';
+
+type SetBodyStepParams = {
+  constant?: string;
+  simple?: string;
+  jq?: string;
+};
+
+export const SetBodyStep = (props: any) => {
+  let initSyntax;
+  let initExpression;
+  if (props.stepParams?.jq) {
+    initSyntax = 'jq';
+    initExpression = props.stepParams?.jq || '';
+  } else if (props.stepParams?.constant) {
+    initSyntax = 'constant';
+    initExpression = props.stepParams?.constant || '';
+  } else {
+    initSyntax = 'simple';
+    initExpression = props.stepParams?.simple || '';
+  }
+
+
+  const [stepParams, setStepParams] = useState<SetBodyStepParams>({
+        constant: props.stepParams?.constant,
+        simple: props.stepParams?.simple,
+        jq: props.stepParams?.jq,
+    });
+
+  function updateKaoto() {
+    props.notifyKaoto('Set Body step updated');
+    props.updateStepParams(stepParams);
+  }
+
+  function updateStepParams(constant?: string, simple?: string, jq?: string) {
+    const newStepParams: SetBodyStepParams = {
+      constant: constant,
+      simple: simple,
+      jq: jq,
+    };
+    setStepParams(newStepParams);
+  }
+
+  return (
+    <Form>
+        <Expression
+          initSyntax={initSyntax}
+          initExpression={initExpression}
+          setExpression={(syntax: string, expression: string) => {
+            const constant = syntax === 'constant' ? expression : undefined;
+            const simple = syntax === 'simple' ? expression : undefined;
+            const jq = syntax === 'jq' ? expression : undefined;
+            updateStepParams(constant, simple, jq);
+          }}
+        />
+
+      <ActionGroup>
+        <Button data-testid='set-body-apply-button' variant="primary" onClick={updateKaoto}>
+          Apply
+        </Button>
+      </ActionGroup>
+    </Form>
+  );
+};
+
+export default SetBodyStep;
