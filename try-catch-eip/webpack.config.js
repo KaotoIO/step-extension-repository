7f6b20220a55072031e2ae646720ba376773973c
kahboom <rachelyordan@gmail.com>
2023-01-26 12:04:20 +0000

    chore(deps): add patternfly support

--- try-catch-eip/webpack.config.js
+++ try-catch-eip/webpack.config.js
@@ -1,61 +1,93 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin =
   require("webpack").container.ModuleFederationPlugin;
-const { dependencies } = require('./package.json');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { dependencies } = require("./package.json");
 const path = require("path");
 const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+
+const isPatternflyStyles = (stylesheet) =>
+  stylesheet.includes("@patternfly/react-styles/css/") ||
+  stylesheet.includes("@patternfly/react-core/");
 
 module.exports = {
   entry: "./src/index",
   mode: "development",
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "dist")
     },
-    port: 3002,
+    port: 3002
   },
   output: {
-    publicPath: "auto",
+    publicPath: "auto"
   },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        include: isPatternflyStyles,
+        use: ["null-loader"],
+        sideEffects: true
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
         test: /\.(tsx|ts|jsx)?$/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
               transpileOnly: true,
-              experimentalWatchApi: true,
-            },
-          },
-        ],
+              experimentalWatchApi: true
+            }
+          }
+        ]
       },
-    ],
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        type: "asset/resource"
+      }
+    ]
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new ModuleFederationPlugin({
       name: "stepextensiondotryeip",
       filename: "remoteEntry.js",
       exposes: {
         "./DoCatch": "./src/DoCatch",
-        "./DynamicInputs": "./src/DynamicInputs",
+        "./DynamicInputs": "./src/DynamicInputs"
       },
-      shared: [{"react": { requiredVersion: dependencies["react"] }}, {"react-dom": { requiredVersion: dependencies["react-dom"] }}],
+      shared: [{ "react": { requiredVersion: dependencies["react"] } }, { "react-dom": { requiredVersion: dependencies["react-dom"] } }, {
+        "@patternfly/patternfly/": {
+          singleton: true,
+          requiredVersion: dependencies["@patternfly/patternfly"]
+        }
+      },
+        {
+          "@patternfly/react-core/": {
+            singleton: true,
+            requiredVersion: dependencies["@patternfly/react-core"]
+          }
+        }]
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+      template: "./public/index.html"
+    })
   ],
   resolve: {
     cacheWithContext: false,
-    extensions: ['.js', '.ts', '.tsx', '.jsx'],
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
     plugins: [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, './tsconfig.json'),
-      }),
+        configFile: path.resolve(__dirname, "./tsconfig.json")
+      })
     ],
-    symlinks: false,
+    symlinks: false
   },
-  stats: 'errors-warnings',
+  stats: "errors-warnings"
 };
