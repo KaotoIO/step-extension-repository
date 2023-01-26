b0fdc74b9f4bbf7fa7e3f7a8f5f986ec446d4420
kahboom <rachelyordan@gmail.com>
2023-01-26 12:29:42 +0000

    feat(ui): add pf components, prettier

--- try-catch-eip/src/App.tsx
+++ try-catch-eip/src/App.tsx
@@ -1,15 +1,31 @@
 // @ts-ignore
 import css from './App.css';
 import { DoCatch } from './DoCatch';
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+import { Page, PageSection, PageSectionVariants } from '@patternfly/react-core';
 
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>This step supports the Java equivalent of try/catch/finally directly in the DSL. It aims to work like its Java sisters but with more power.</p>
-      </header>
-      <DoCatch />
-      <footer className="App-footer">When using this EIP, the regular Camel Error Handler is not in use.</footer>
+      <Page>
+        <PageSection variant={PageSectionVariants.light}>
+          <header className="App-header">
+            <p>
+              This step supports the Java equivalent of try/catch/finally directly in the DSL. It
+              aims to work like its Java sisters but with more power.
+            </p>
+          </header>
+          <DoCatch />
+          <footer className="App-footer">
+            When using this EIP, the regular Camel Error Handler is not in use.
+          </footer>
+        </PageSection>
+      </Page>
     </div>
   );
-}
+};
