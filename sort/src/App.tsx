5628910a5634743c7c98adfc7e1ea2e739586430
tplevko <tplevko@redhat.com>
2023-05-19 13:17:59 +0200

    Add sort step extension

---
+++ sort/src/App.tsx
@@ -0,0 +1,21 @@
+import SortStep from "./Components/SortStep";
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        </p>
+      </header>
+      <SortStep />
+    </div>
+  );
+}
+
+export default App;
