a87f1ccf30d0efb1c4b3025b2112bf74f80f4bb5
Maria Arias de Reyna <ariasdereyna@redhat.com>
2023-05-26 11:06:20 +0200

    feature(rest): Convert component to function

--- rest-dsl/src/Components/MimeTypes.tsx
+++ rest-dsl/src/Components/MimeTypes.tsx
@@ -1,9 +1,8 @@
 import { Select, SelectOption, SelectVariant } from '@patternfly/react-core';
 import * as defaultMimeTypes from './DefaultMimeTypes';
-import React from 'react';
+import { useState } from 'react';
 
 //Code strongly "inspired" from https://www.patternfly.org/v4/components/select/#typeahead
-
 export interface IMimeTypeSelectInputForm {
   label: string;
   onChange: any;
@@ -19,91 +18,63 @@ export interface IMimeTypeStatus {
   newOptions: JSX.Element[];
 }
 
-class MimeTypeSelectInput extends React.Component<IMimeTypeSelectInputForm, IMimeTypeStatus> {
-  onToggle: (isOpen: any) => void;
-  onSelect: (event: any, selection: any, isPlaceholder: any) => void;
-  onCreateOption: (newValue: any) => void;
-  constructor(props: IMimeTypeSelectInputForm) {
-    super(props);
+export const MimeTypeSelectInput = ({ label, onChange, value, values }: IMimeTypeSelectInputForm) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<string | undefined>(value);
 
-    this.state = {
-      isOpen: false,
-      selected: props.value,
-      label: props.label,
-      values: props.values,
-      newOptions: []
-    };
+  const onToggle = (isNowOpen: boolean) => {
+    setIsOpen(isNowOpen);
+  };
 
-    this.onToggle = isOpen => {
-      this.setState({
-        isOpen
-      });
-    };
+  // @ts-ignore
+  const onSelect = (event, selection, isPlaceholder) => {
+    if (isPlaceholder) {
+      clearSelection();
+    }
+    else {
+      setSelected(selection);
+      setIsOpen(false);
+    }
+    onChange(selection);
+  };
 
-    // @ts-ignore
-    this.onSelect = (event, selection, isPlaceholder) => {
-      if (isPlaceholder) this.clearSelection();
-      else {
-        this.setState({
-          selected: selection,
-          isOpen: false
-        });
+  const clearSelection = () => {
+    setSelected(undefined);
+    setIsOpen(false);
+  };
+
+  let mimetypes: string[] = values;
+
+  if (!mimetypes || mimetypes.length == 0) {
+    mimetypes = defaultMimeTypes.DEFAULT_MIME_TYPES;
+  }
+
+
+  if (value && !mimetypes.includes(value)) {
+    //Put the value on top
+    mimetypes = [value].concat(mimetypes);
+  }
+
+  return (
+    <Select
+      variant={SelectVariant.typeahead}
+      typeAheadAriaLabel={label}
+      onToggle={onToggle}
+      onSelect={onSelect}
+      onClear={clearSelection}
+      selections={selected}
+      isOpen={isOpen}
+      aria-labelledby={label}
+      placeholderText="application/json"
+      isCreatable={true}
+    >
+      {
+        mimetypes.map((v) =>
+          <SelectOption selected={v === selected} key={v} value={v} />
+        )
       }
-      props.onChange(selection);
-    };
-
-    this.onCreateOption = newValue => {
-      this.setState({
-        newOptions: [...this.state.newOptions, <SelectOption key={newValue} value={newValue} />]
-      });
-    };
-
-    this.clearSelection = () => {
-      this.setState({
-        selected: undefined,
-        isOpen: false
-      });
-    };
-  }
-  clearSelection() {
-    throw new Error('Method not implemented.');
-  }
-
-  render() {
-    const { isOpen, selected, label, values } = this.state;
-    let mimetypes: string[] = values;
-
-    if (!mimetypes || mimetypes.length == 0) {
-      mimetypes = defaultMimeTypes.DEFAULT_MIME_TYPES;
-    }
-
-
-    if (selected && !mimetypes.includes(selected)) {
-      //Put the value on top
-      mimetypes = [selected].concat(mimetypes);
-    }
-
-    return (
-      <Select
-        variant={SelectVariant.typeahead}
-        typeAheadAriaLabel={label}
-        onToggle={this.onToggle}
-        onSelect={this.onSelect}
-        onClear={this.clearSelection}
-        selections={selected}
-        isOpen={isOpen}
-        aria-labelledby={label}
-        placeholderText="application/json"
-        isCreatable={true}
-      >
-        {
-          mimetypes.map((v) =>
-            <SelectOption selected={v === selected} key={v} value={v} />
-          )
-        }
-      </Select>
-    );
-  }
+    </Select>
+  );
 }
 
 export const MimeTypes = ({ label, onChange, value, values }: IMimeTypeSelectInputForm) => {
