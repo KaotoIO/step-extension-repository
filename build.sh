56bba6b97713dcaeaab054a7fb0630b29b3ff08f
tplevko <tplevko@redhat.com>
2023-06-01 14:18:32 +0200

    Exclude common workspace from creating dist

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yarn install --immutable
 yarn workspaces foreach --exclude "@kaoto/step-extension-repository" --parallel --verbose run clean
-yarn workspaces foreach --exclude "@kaoto/step-extension-repository" --parallel --verbose run build
+yarn workspaces foreach --exclude "@kaoto/step-extension-repository" --topological-dev --parallel --verbose run build
 
 rm -rf ./dist
 mkdir -p ./dist
 
-yarn workspaces foreach --exclude "@kaoto/step-extension-repository" --parallel --verbose exec bash -c 'working_folder=${PWD##*/}; cp -r "${PWD}/dist/" "../dist/${working_folder}"'
+yarn workspaces foreach --exclude "@kaoto/step-extension-repository" --exclude common --parallel --verbose exec bash -c 'working_folder=${PWD##*/}; cp -r "${PWD}/dist/" "../dist/${working_folder}"'
